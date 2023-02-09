c8e5a09f4eb9f05e422bc2d6e5a678b853a60c82
David Lopes <davidllopes@gmail.com>
2023-02-09 15:51:59 +0000

    added basic example

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Scene from "./scenes/Scene";
+
+export default function App() {
+    return (
+        <div className="App ">
+            <div className="min-h-screen w-full h-1">
+                <Scene />
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { sendToVercelAnalytics } from "./vitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 reportWebVitals(sendToVercelAnalytics);
